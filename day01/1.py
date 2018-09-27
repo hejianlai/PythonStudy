e1a6407040475432f5d0b749ee709698e989cd43
hejianlai <31337851+hejianlai@users.noreply.github.com>
2018-09-27 22:42:55 +0800

    delete
    delete

--- day01/1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-a = input()
-print(a)
-print("你好")
-print(a * a )
--- day01/dict.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-a ={'hejianlai':'34''23','root':'333'}
-print(a['hejianlai'])
-
-'hejianlai' in a
